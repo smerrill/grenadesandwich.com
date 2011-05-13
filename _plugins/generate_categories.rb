6892fc455a56d4e54105d90ee9d320873f5f7a74
Steven Merrill <steven.merrill@gmail.com>
2011-05-13 13:39:05 -0400

    Convert spaces to dashes in category names.

--- _plugins/generate_categories.rb
+++ _plugins/generate_categories.rb
@@ -94,7 +94,8 @@ module Jekyll
       if self.layouts.key? 'category_index'
         dir = self.config['category_dir'] || 'categories'
         self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category), category)
+          # Spaces to dashes.
+          self.write_category_index(File.join(dir, category.gsub(' ', '-')), category)
         end
 
         # Throw an exception if the layout couldn't be found.
